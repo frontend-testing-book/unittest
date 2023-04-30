2f0b32dfb2f7e60e93eaa8cc734942f3ad51c24f
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2023-04-30 12:31:31 +0900

    refactor: 画面遷移前にログを送信する

--- src/05/04/ArticleListItem.tsx
+++ src/05/04/ArticleListItem.tsx
@@ -4,12 +4,31 @@ export type ItemProps = {
   body: string;
 };
 
+async function postLog() {
+  // ログを送信する処理
+  return;
+}
+
 export const ArticleListItem = ({ id, title, body }: ItemProps) => {
   return (
     <li>
       <h3>{title}</h3>
       <p>{body}</p>
-      <a href={`/articles/${id}`}>もっと見る</a>
+      <a
+        style={{
+          color: "blue",
+          cursor: "pointer",
+          textDecoration: "underline",
+        }}
+        onClick={async (event) => {
+          const href = `/articles/${id}`;
+          event.preventDefault();
+          await postLog();
+          window.location.assign(href);
+        }}
+      >
+        もっと見る
+      </a>
     </li>
   );
 };
