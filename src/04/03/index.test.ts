70dfc30cf3145589804462a3ce0d66b27bdef907
asano <asanokeenta@me.com>
2023-05-13 23:26:32 +0900

    成功時テスト追加

--- src/04/03/index.test.ts
+++ src/04/03/index.test.ts
@@ -10,8 +10,9 @@ describe("getGreet", () => {
     jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
       id: "xxxxxxx-123456",
       email: "taroyamada@myapi.testing.com",
+      name: "asano",
     });
-    await expect(getGreet()).resolves.toBe("Hello, anonymous user!");
+    await expect(getGreet()).resolves.toBe("Hello, asano!");
   });
   test("データ取得成功時：ユーザー名がある場合", async () => {
     jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
