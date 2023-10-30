58ea57c1fda07c5618e56d1201990ba2f2ca8d28
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 11:59:17 +0900

    🎨 フォーマット

--- src/03/02/index.test.ts
+++ src/03/02/index.test.ts
@@ -1,5 +1,7 @@
-import { add } from "."
+import { add } from '.'
 
-test("add: 1 + 2 は 3", () => {
-  expect(add(1, 2)).toBe(3)
+describe('add', () => {
+  test('1 + 1 は 2', () => expect(add(1, 1)).toBe(2))
+
+  test('1 + 2 は 3', () => expect(add(1, 2)).toBe(3))
 })
--- src/03/02/index.ts
+++ src/03/02/index.ts
@@ -1,7 +1,7 @@
 export function add(a: number, b: number) {
-  return a + b;
+  return a + b
 }
 
 export function sub(a: number, b: number) {
-  return a - b;
+  return a - b
 }
