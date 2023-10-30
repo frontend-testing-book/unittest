6ce9b268d3a6d374e8e3207570c29a673a6e2cd9
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 13:07:27 +0900

    ✅ try-catchを用いたrejectのテスト

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -6,3 +6,12 @@ test('指定時間待つと、経過時間をもってresolvesされる', async 
 test('指定時間待つと、経過時間をもってrejectされる', async () => {
   await expect(timeout(50)).rejects.toBe(50)
 })
+
+test('指定時間待つと、経過時間をもってrejectされる', async () => {
+  expect.assertions(1)
+  try {
+    await timeout(50)
+  } catch (err) {
+    expect(err).toBe(50)
+  }
+})
