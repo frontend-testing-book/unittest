cd89d9ea21252369e2e5d330d656b86325662ae8
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 12:09:23 +0900

    ✅ describeについて

--- src/03/02/index.test.ts
+++ src/03/02/index.test.ts
@@ -1,7 +1,15 @@
-import { add } from '.'
+import { add, sub } from '.'
 
-describe('add', () => {
-  test('1 + 1 は 2', () => expect(add(1, 1)).toBe(2))
+describe('四則演算', () => {
+  describe('add', () => {
+    test('1 + 1 は 2', () => expect(add(1, 1)).toBe(2))
 
-  test('1 + 2 は 3', () => expect(add(1, 2)).toBe(3))
+    test('1 + 2 は 3', () => expect(add(1, 2)).toBe(3))
+  })
+
+  describe('sub', () => {
+    test('1 - 1 は 0', () => expect(sub(1, 1)).toBe(0))
+
+    test('2 - 1 は 1', () => expect(sub(2, 1)).toBe(1))
+  })
 })
