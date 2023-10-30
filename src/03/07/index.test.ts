f30a781058eba3fe4e404f59e27b79a78d94ae12
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 13:04:41 +0900

    ✅ rejectのテスト

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -1,4 +1,8 @@
-import { wait } from '.'
+import { timeout, wait } from '.'
 
 test('指定時間待つと、経過時間をもってresolvesされる', async () =>
   expect(await wait(50)).toBe(50))
+
+test('指定時間待つと、経過時間をもってrejectされる', () => {
+  return timeout(50).catch((duration) => expect(duration).toBe(50))
+})
