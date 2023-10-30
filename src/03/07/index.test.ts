2ad5bf61201812d4269538af50277b03b52b034a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 13:06:21 +0900

    ✅ async・awaitを用いたrejectのテスト

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -3,6 +3,6 @@ import { timeout, wait } from '.'
 test('指定時間待つと、経過時間をもってresolvesされる', async () =>
   expect(await wait(50)).toBe(50))
 
-test('指定時間待つと、経過時間をもってrejectされる', () => {
-  return timeout(50).catch((duration) => expect(duration).toBe(50))
+test('指定時間待つと、経過時間をもってrejectされる', async () => {
+  await expect(timeout(50)).rejects.toBe(50)
 })
