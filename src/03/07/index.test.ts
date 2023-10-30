e33f2acb78aa22de07d2a86fdb6a60457a3361fe
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 13:00:54 +0900

    ✅ async・awaitを使用したテスト

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -1,5 +1,4 @@
 import { wait } from '.'
 
-test('指定時間待つと、経過時間をもってresolvesされる', () => {
-  return expect(wait(50)).resolves.toBe(50)
-})
+test('指定時間待つと、経過時間をもってresolvesされる', async () =>
+  await expect(wait(50)).resolves.toBe(50))
